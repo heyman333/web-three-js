784b214b5635b57d80a2ced9a8b8b3690cc0a76c
lake <lake.zero@kakaostyle.com>
2025-05-19 22:53:25 +0900

    feat: lake

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './react/joshua/App';
+import App from './react/lake/scroll-animation/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
